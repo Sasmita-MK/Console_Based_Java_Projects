74fc830558f5fc44d97ee9e848a298ca17ff3f8c
Sasmita-MK <sasmitamayilsamy@gmail.com>
2024-11-26 11:08:20 +0530

    Changed the name of the app for both projects

--- Tic_Tac_Toe/src/TicTacToeApp.java
+++ Tic_Tac_Toe/src/TicTacToeApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class TicTacToeApp {
     public static void main(String[] args) throws Exception {
         System.out.println("Tic Tac Toe");
 
--- Vending_Machine/src/VendingMachineApp.java
+++ Vending_Machine/src/VendingMachineApp.java
@@ -1,4 +1,4 @@
-public class App {
+public class VendingMachineApp {
     public static void main(String[] args) throws Exception {
         Vending_Machine vending_Machine = new Vending_Machine();
         vending_Machine.logic();
